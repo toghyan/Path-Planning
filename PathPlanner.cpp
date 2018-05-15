1278d97325e0a3b3793cf41220a61652f246411a
Aliakbar Toghyan <ali.toghyan@gmail.com>
2018-05-14 21:02:47 -0700

    RSR Done!

--- PathPlanner.cpp
+++ PathPlanner.cpp
@@ -31,14 +31,31 @@ vector<double> PathPlanner::RSR(){
     
     double alpha = atan2(y_center_end - y_center_start, x_center_end - x_center_start);
 
+    // Angle of car at the start circle
+    double theta_start = atan2(car_start.get_y() - y_center_start, car_start.get_x() - x_center_start);
+
     // Angle of rotation on start circle
-    double theta_start = atan2(car_start.get_y() - y_center_start, car_start.get_x() - x_center_start) - alpha - M_PI / 2;
-    // Angle of rotation on start circle
-    double theta_end = alpha + M_PI / 2 - atan2(car_end.get_y() - y_center_end, car_end.get_x() - x_center_end);
-    
-    path[0] = theta_start * r_min;
+    double theta_end = atan2(car_end.get_y() - y_center_end, car_end.get_x() - x_center_end);
+    // Amount of rotation on the start circle 
+    double Rotation_C1 = theta_start - alpha - M_PI_2;
+    if(Rotation_C1 < 0){
+        Rotation_C1 += 2 * M_PI;
+    } 
+    else if(Rotation_C1 > 2 * M_PI) {
+        Rotation_C1 -= 2 * M_PI;
+    }
+    // Amount of rotation on the end circle
+    double Rotation_C2 = alpha + M_PI_2 - thata_end;
+    if(Rotation_C2 < 0){
+        Rotation_C2 += 2 * M_PI;
+    }
+    else if(Rotation_C2 > 2 * M_PI) {
+        Rotation_C2 -= 2 * M_PI;
+    }
+ 
+    path[0] = Rotation_C1 * r_min;
     path[1] = sqrt(pow(x_center_end - x_center_start,2) + pow(y_center_end - y_center_start,2));
-    path[3] = theta_end * r_min;
+    path[3] = Rotation_C2 * r_min;
     
     return path;
 }
