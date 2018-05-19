197a061e80183b5f0cd1a3f0b06d9ea10392a248
Aliakbar Toghyan <ali.toghyan@gmail.com>
2018-05-19 13:02:53 -0700

    Modifying RRT

--- RRT.cpp
+++ RRT.cpp
@@ -4,7 +4,27 @@ RRT::RRT()
 {
 }
 
-RRT::~RRT()
-{
+RRT::RRT(Car start_car, Car goal_car, double size_of_world, double radius_goal, double delta_time){
+    start = start_car;
+    goal = goal_car;
+    world_size = size_of_world;
+    r_goal = radius_goal;
+    dt = delta_time;
 }
 
+RRT::~RRT()
+{
+    
+}
+
+*Car RRT::Random_Car(){
+    
+}
+
+*Car RRT::find_nearest(Car end){
+    
+}
+
+bool RRT::expand_tree(){
+    
+}
--- RRT.h
+++ RRT.h
@@ -12,20 +12,21 @@ public:
     RRT();
     // Constructor. Using start position of the car and defining the end goal as a circle. The size of the world is also
     // Part of the constructor. It defines the limit on x and y coordinate as -size_of_world =< x,y =< size_of_world
-    RRT(Car start, Car goal, double size_of_world, double radius_goal, double delta_time);
+    RRT(Car start_car, Car goal_car, double size_of_world, double radius_goal, double delta_time);
     // Deconstructor
     ~RRT();
     // Generates random Car postions in the boundaries of the configuration space
-    Car Random_Car();
+    *Car Random_Car();
     // Finds the neerest Car in the tree to the input position
     *Car find_nearest(Car end);
-    // Expands the tree
-    void expand_tree();
+    // Expands the tree and checks if the added node is in the goal zone
+    bool expand_tree();
     
     
     
 Private:
-    double world_size;
+    double world_size, dt, r_goal;
+    Car start, goal; 
     
 
 };
